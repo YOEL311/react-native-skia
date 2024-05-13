913a6ab28bc391ee3da4fef1389aa2a6dce77194
William Candillon <wcandillon@gmail.com>
2024-05-13 08:13:52 +0200

    :wrench:

--- example/src/Examples/Video/index.ts
+++ example/src/Examples/Video/index.ts
@@ -1,3 +1,4 @@
 //export { Transitions as Video } from "./Transitions";
 //export { Stories as Video } from "./Stories";
-export { Stickers as Video } from "./Stickers";
+//export { Stickers as Video } from "./Stickers";
+export { Riveo as Video } from "./Riveo";
